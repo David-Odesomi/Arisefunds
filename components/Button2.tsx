c7b4287f35f4b2e592312866d2169d90e3b6ce71
David Odesomi <davidodesomi@gmail.com>
2026-04-23 12:05:48 +0100

    Added Small banner

---
+++ components/Button2.tsx
@@ -0,0 +1,9 @@
+export default function MyComponent() {
+    return (
+        <div className="mt-4">
+            <button className="bg-white text-[#1096DC] px-8 py-3 text-sm font-medium hover:bg-[#149EED]/90 transition-colors">
+              Get intouch
+            </button>
+          </div>
+    );
+}
